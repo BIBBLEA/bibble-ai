93e90693ea50ca974da5d4f599c5da60b1b1c113
BIBBLEA <3.04019191e+08+BIBBLEA@users.noreply.github.com>
2026-08-01 11:04:42 +0000

    Fix: Rename next.config.ts to next.config.mjs to support build on Vercel

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["files.heygen.ai", "files2.heygen.ai", "files3.heygen.ai"],
+  },
+};
+
+export default nextConfig;
